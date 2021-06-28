826bf235eb2bfb3d9f1591dd25e44d378f8dcb7b
monica <monica.getnea.com>
2021-06-28 15:29:08 -0500

    Se agrega codigo para autentificar las vistas de las apps

--- musica/canciones/views.py
+++ musica/canciones/views.py
@@ -3,8 +3,23 @@ from rest_framework import viewsets
 from .models import Cancion, Autor, Album
 from .serializers import CancionSerializer, AutorSerializer, AlbumSerializer
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 class CancionViewSet(viewsets.ModelViewSet):
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+        content = {
+            'user': str(request.user),  # `django.contrib.auth.User` instance.
+            'auth': str(request.auth),  # None
+        }
+        return Response(content)
+
     serializer_class = CancionSerializer
     queryset = Cancion.objects.all()
 
