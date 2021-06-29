927cba05ed506dd9384f46c9a71599582dbfa475
monica <monica.getnea.com>
2021-06-28 23:22:58 -0500

    Se corrige la doble implementacion de los esquemas de autentificacion SessionAuthentication y BasicAuthentication eliminando estos en los archivos de vistas de las apps y dejando solo su implementacion de forma global; Se elimina la funcion get donde se obtenian las credenciales del usuario logueado ya que no se utilizan para este codigo asi como tambien las librerias que se necesitaban para su funcionamiento

--- musica/disquera/views.py
+++ musica/disquera/views.py
@@ -3,22 +3,11 @@ from rest_framework import viewsets
 from .models import Disquera
 from .serializers import DisqueraSerializer
 
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 
 class DisqueraViewSet(viewsets.ModelViewSet):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
     permission_classes = (IsAuthenticated,)
-
-    def get(self, request, format=None):
-        content = {
-            'user': str(request.user),  # `django.contrib.auth.User` instance.
-            'auth': str(request.auth),  # None
-        }
-        return Response(content)
 
     serializer_class = DisqueraSerializer
     queryset = Disquera.objects.all()
